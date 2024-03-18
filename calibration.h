71499c161ed8c73b2bf362cfb3c1e79f1e55d118
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-03-18 14:11:06 -0600

    Create calibration.h

---
+++ calibration.h
@@ -0,0 +1,8 @@
+#ifndef CALIBRATION_H
+#define CALIBRATION_H
+
+#define CALIBRATION_SAMPLES 100
+
+void calibrate_offsets(float *x_offset, float *y_offset, float *z_offset);
+
+#endif // CALIBRATION_H
