be73f41bd562dc7f1d73d5fb6c7f511b4c4ba89b
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-03-28 21:17:32 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,40 @@
+#include "external_temp_humidity_h.h"
 #include "acceleration.h"
 #include "calibration.h"
 #include <stdio.h>
 #include <unistd.h>
 
-#define FILENAME "/mnt/sdcard/accelerometer.txt"
+#define FILENAME "/mnt/sdcard/sensors.txt"
 
 int main() {
-    // Initialize accelerometer
     init_accelerometer();
 
-    // Calibrate accelerometer
     float x_offset, y_offset, z_offset;
     calibrate_offsets(&x_offset, &y_offset, &z_offset);
 
-    // Open file for appending
     FILE *file = fopen(FILENAME, "a");
     if (file == NULL) {
-        perror("Failed to open file for writing acceleration data.");
+        perror("Failed to open file for writing data.");
         return 1;
     }
 
-    // Main loop to continuously read acceleration data
     while (1) {
-        // Read acceleration values from each axis
+        float temperature, humidity;
+        external_temp_humidity(&temperature, &humidity);
+
         float x_accel = read_acceleration_x() - x_offset;
         float y_accel = read_acceleration_y() - y_offset;
         float z_accel = read_acceleration_z() - z_offset;
 
-        // Save acceleration data to file
+        fprintf(file, "Temperature: %.2f°C\n", temperature);
+        fprintf(file, "Humidity: %.2f%%\n", humidity);
         fprintf(file, "Acceleration in X-Axis: %.2f g\n", x_accel);
         fprintf(file, "Acceleration in Y-Axis: %.2f g\n", y_accel);
         fprintf(file, "Acceleration in Z-Axis: %.2f g\n", z_accel);
 
-        // Add a delay to control the frequency of the readings
         usleep(100000);  // Delay for 100 milliseconds
     }
 
-    // Close the file
     fclose(file);
-
     return 0;
 }
