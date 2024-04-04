d88f02242682fbb35d3a4161a5c0597cab2fa964
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-03 20:33:06 -0600

    Update door.c

--- door.c
+++ door.c
@@ -4,9 +4,23 @@
 #include <string.h>
 
 #define GPIO_EXPORT_PATH "/sys/class/gpio/export"
-#define SENSOR_PIN "60"
-#define LED_PIN "66"
 #define GPIO_PATH_PREFIX "/sys/class/gpio/gpio"
+
+void export_gpio(const char *gpio);
+void set_direction(const char *gpio, const char *direction);
+char read_value(const char *gpio);
+void set_value(const char *gpio, const char *value);
+
+void setup_gpio()
+{
+    // Export GPIO pins
+    export_gpio(SENSOR_PIN);
+    export_gpio(LED_PIN);
+
+    // Set GPIO pin directions
+    set_direction(SENSOR_PIN, "in");
+    set_direction(LED_PIN, "out");
+}
 
 void export_gpio(const char *gpio)
 {
@@ -84,37 +98,4 @@ void set_value(const char *gpio, const char *value)
     }
 
     close(value_fd);
-}
-
-int main()
-{
-    // Export GPIO pins
-    export_gpio(SENSOR_PIN);
-    export_gpio(LED_PIN);
-
-    // Set GPIO pin directions
-    set_direction(SENSOR_PIN, "in");
-    set_direction(LED_PIN, "out");
-
-    // Variables for reading sensor value
-    char sensor_value;
-
-    while (1)
-    {
-        // Read sensor value
-        sensor_value = read_value(SENSOR_PIN);
-
-        // Debug print statement
-        printf("Sensor Value: %c\n", sensor_value);
-
-        // Control the LED based on sensor value
-        if (sensor_value == '0') // Magnet detected
-            set_value(LED_PIN, "0"); // Turn off the LED
-        else
-            set_value(LED_PIN, "1"); // Turn on the LED
-
-        usleep(100000); // Delay for stability (100ms)
-    }
-
-    return 0;
 }
