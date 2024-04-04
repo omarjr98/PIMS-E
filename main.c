63ac6e1f1401cd5a8918cc12ed591a736d087846
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-03 20:38:41 -0600

    Update main.c

--- main.c
+++ main.c
@@ -16,6 +16,10 @@ int main() {
     if (file == NULL) {
         perror("Failed to open file for writing data.");
         return 1;
+    setup_gpio();
+
+    // Variables for reading sensor value
+    char sensor_value;
     }
 
     while (1) {
@@ -36,9 +40,20 @@ int main() {
         fprintf(file, "Acceleration in X-Axis: %.2f g\n", x_accel);
         fprintf(file, "Acceleration in Y-Axis: %.2f g\n", y_accel);
         fprintf(file, "Acceleration in Z-Axis: %.2f g\n", z_accel);
+       
+        sensor_value = read_value(SENSOR_PIN);
 
+        // Debug print statement
+        printf("Sensor Value: %c\n", sensor_value);
+
+        // Control the LED based on sensor value
+        if (sensor_value == '0') // Magnet detected
+            set_value(LED_PIN, "0"); // Turn off the LED
+        else
+            set_value(LED_PIN, "1"); // Turn on the LED
         usleep(100000);  // Delay for 100 milliseconds
     }
+   
 
     fclose(file);
     return 0;
