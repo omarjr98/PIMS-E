35e3599f98496c0586919756d51168f1fd141cf3
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-06 03:28:27 -0600

    Update wind_sensor.h

--- wind_sensor.h
+++ wind_sensor.h
@@ -2,6 +2,7 @@
 #define wind_sensor_h
 
 // Function declaration for reading wind speed
-void read_wind_speed();
+void read_wind_speed(float *Vtemp, float *wind_speed_mph, float *TEMP);
+void init_i2c();
 
 #endif
