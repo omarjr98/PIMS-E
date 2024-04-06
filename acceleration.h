a404c65dbec4f2fb6e5bbb1218962cab5cc9d3bc
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-06 03:36:17 -0600

    Update acceleration.h

--- acceleration.h
+++ acceleration.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h> // Include for int16_t type
 
-void init_accelerometer();
+void init_accelerometer(float *x_accel_raw , float *y_accel_raw , float *z_accel_raw);
 float read_acceleration_x();
 float read_acceleration_y();
 float read_acceleration_z();
