57616ffa8d86a6d450db87ef417756fbd8330be3
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-08 15:16:27 -0600

    Update accelerometer.h

--- acceleration.h
+++ acceleration.h
@@ -1,12 +1,8 @@
-#ifndef ACCELERATION_H
-#define ACCELERATION_H
+#ifndef ACCELEROMETER_H
+#define ACCELEROMETER_H
 
-#include <stdint.h> // Include for int16_t type
+void initialize_accelerometer(int *file);
+void read_acceleration(float file, float *xAccl, float *yAccl, float *zAccl);
 
-void init_accelerometer(float *x_accel_raw , float *y_accel_raw , float *z_accel_raw);
-float read_acceleration_x();
-float read_acceleration_y();
-float read_acceleration_z();
-int16_t read_signed16(int reg); // Add function prototype here
+#endif /* ACCELEROMETER_H */
 
-#endif // ACCELERATION_H
