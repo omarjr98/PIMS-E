04ce14bc40ae9e4d2da2b4c0732cabeeee9991dc
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-09 01:01:01 -0600

    Update main.c
    we add definition for time

--- main.c
+++ main.c
@@ -14,12 +14,13 @@
 
 #define RTC_DEVICE "/dev/rtc1"
 #define FILENAME "/mnt/sdcard/testing.txt"
-
+#define DATETIME_LENGTH 20
 
 int main() {
 
-	int file, rtc_fd;
+    int file, rtc_fd;
     struct rtc_time rtc_tm;
+    char dateTime[DATETIME_LENGTH];
 
     // Accelerometer calibration
     initialize_accelerometer(&file);
@@ -53,6 +54,10 @@ int main() {
 
         close(rtc_fd);
 
+        // Get current date and time
+        snprintf(dateTime, DATETIME_LENGTH, "%04d-%02d-%02d %02d:%02d:%02d",
+                 rtc_tm.tm_year + 1900, rtc_tm.tm_mon + 1, rtc_tm.tm_mday,
+                 rtc_tm.tm_hour, rtc_tm.tm_min, rtc_tm.tm_sec);
 
         // Read internal temperature and humidity
         internal_temp_humidity(&internal_temperature, &internal_humidity);
@@ -61,37 +66,37 @@ int main() {
         external_temp_humidity(&external_temperature, &external_humidity);
 
         //Read acceleration
-    	read_acceleration(file, &xAccl, &yAccl, &zAccl);
-    		//Remove acceleration offsets
-               xAccl -= xOffset;
-               yAccl -= yOffset;
-               zAccl -= zOffset;
+        read_acceleration(file, &xAccl, &yAccl, &zAccl);
+        //Remove acceleration offsets
+        xAccl -= xOffset;
+        yAccl -= yOffset;
+        zAccl -= zOffset;
 
         //Read wind speed
-    	read_wind_speed(&Vtemp, &wind_speed_mph, &TEMP);
+        read_wind_speed(&Vtemp, &wind_speed_mph, &TEMP);
 
 
         // Print sensor data to terminal
-        printf("%04d-%02d-%02d %02d:%02d:%02d\n",rtc_tm.tm_year + 1900, rtc_tm.tm_mon + 1, rtc_tm.tm_mday,rtc_tm.tm_hour, rtc_tm.tm_min, rtc_tm.tm_sec);
+        printf("%s\n", dateTime);
         printf("Internal Temperature: %.2f C\nInternal Humidity: %.2f%\n", internal_temperature, internal_humidity);
         printf("External Temperature: %.2f C\nInternal Humidity: %.2f%\n", external_temperature, external_humidity);
         printf("Acceleration X: %.2f g Y: %.2f g Z: %.2f g\n", xAccl, yAccl, zAccl);
-        printf("Wind Speed: %.2f MPH\n",wind_speed_mph );
+        printf("Wind Speed: %.2f MPH\n", wind_speed_mph);
 
 
         // Write sensor data to memory bank
-        FILE *file = fopen(FILENAME, "a");
-        if (file == NULL) {
+        FILE *outputFile = fopen(FILENAME, "a");
+        if (outputFile == NULL) {
             perror("Failed to open file for writing data to SD Card.");
             return 1;
         }
 
-        fprintf(file, "%04d-%02d-%02d %02d:%02d:%02d",rtc_tm.tm_year + 1900, rtc_tm.tm_mon + 1, rtc_tm.tm_mday,rtc_tm.tm_hour, rtc_tm.tm_min, rtc_tm.tm_sec);
-        fprintf(file, " %.2fC %.2f%%", internal_temperature, internal_humidity);
-        fprintf(file, " %.2fC %.2f%%", external_temperature, external_humidity);
-        fprintf(file, " X:%.2fg Y:%.2fg Z:%.2fg", xAccl, yAccl, zAccl);
-        fprintf(file, " %.2fMPH\n",wind_speed_mph );
-        fclose(file);
+        fprintf(outputFile, " %s", dateTime);
+        fprintf(outputFile, " %.2fC %.2f%%", internal_temperature, internal_humidity);
+        fprintf(outputFile, " %.2fC %.2f%%", external_temperature, external_humidity);
+        fprintf(outputFile, " X:%.2fg Y:%.2fg Z:%.2fg", xAccl, yAccl, zAccl);
+        fprintf(outputFile, " %.2fMPH\n", wind_speed_mph);
+        fclose(outputFile);
 
         // Delay for 1 second before next reading
         sleep(1);
