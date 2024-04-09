33f73af1cd0f239a74a2bfa2be78a65b3646e134
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-08 18:30:33 -0600

    Update external_temp_humidity.c

--- external_temp_humidity.c
+++ external_temp_humidity.c
@@ -14,10 +14,10 @@
 #define SENSOR_ADDR 0x44
 
 // File path for uSD card
-#define FILENAME "/mnt/sdcard/temp_humid.bin"
+
 
 // Main function
-void external_temp_humidity() {
+void external_temp_humidity(float *external_temperature, float *external_humidity) {
     int external_temp_humidity_fd; // Local file descriptor
 
     // Calls the I2C BUS to open
@@ -50,33 +50,16 @@ void external_temp_humidity() {
     }
 
     // Process sensor data and write to file
-    float temperature = -45 + 175.0 * ((buffer[0] << 8 | buffer[1]) / 65535.0); // Convert raw temperature to Celsius
-    float humidity = 100.0 * ((buffer[3] << 8 | buffer[4]) / 65535.0); // Convert humidity to percentage
+     *external_temperature = -45 + 175.0 * ((buffer[0] << 8 | buffer[1]) / 65535.0); // Convert raw temperature to Celsius
+     *external_humidity = 100.0 * ((buffer[3] << 8 | buffer[4]) / 65535.0); // Convert humidity to percentage
 
   // Print on terminal
-  printf("External Temperature: %.2f degC\n", temperature);
-  printf("External Humidity: %.2f %%\n", humidity);
+ // printf("External Temperature: %.2f degC\n", external_temperature);
+  //printf("External Humidity: %.2f %%\n", external_humidity);
 
-    FILE *file = fopen(FILENAME, "a"); // Open file for appending
-    if (file == NULL) {
-        perror("Failed to open file for writing external temperature and humidity data");
-        close(external_temp_humidity_fd);
-        exit(1);
-    }
 
-    // Write binary data to the file
-    char binary_data[64]; // Data size
-    int index = 0;
 
-  // Convert temperature and humidity to 32-bit binary with 10 integer bits and 22 fractional bits
-  unsigned int temperature_integer = (unsigned int)temperature;
-  unsigned int temperature_fractional = (unsigned int)((temperature - temperature_integer) * (1 << 22));
-  unsigned int humidity_integer = (unsigned int)humidity;
-  unsigned int humidity_fractional = (unsigned int)((humidity - humidity_integer) * (1 << 22));
-  unsigned int data[] = {temperature_integer, temperature_fractional, humidity_integer, humidity_fractional};
-  fwrite(data, sizeof(unsigned int), sizeof(data) / sizeof(unsigned int), file);
-  fprintf(file, "\n");
 
-    fclose(file);
+
     close(external_temp_humidity_fd);
 }
