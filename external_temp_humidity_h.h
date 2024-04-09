84842e449467fc515b1cc07f29c8f32590f9a4d4
omarjr98 <91510111+omarjr98@users.noreply.github.com>
2024-04-08 18:30:52 -0600

    Update external_temp_humidity_h.h

--- external_temp_humidity_h.h
+++ external_temp_humidity_h.h
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 // Function declaration
-void external_temp_humidity();
+void external_temp_humidity(float *external_temperature, float *external_humidity);
+
 void init_i2c();
 
 #endif
