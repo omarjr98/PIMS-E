337c48d1233dcadd5d7adb375c4c133a76175776
KTurton <159364354+KTurton@users.noreply.github.com>
2024-04-08 21:44:27 -0600

    Update and rename external_temp_humidity_h.h to external_temp_humidity.h
    Cleanout

---
+++ external_temp_humidity.h
@@ -0,0 +1,10 @@
+#ifndef EXTERNAL_TEMP_HUMIDITY_H
+#define EXTERNAL_TEMP_HUMIDITY_H
+#include <stdio.h>
+
+
+void external_temp_humidity(float *external_temperature, float *external_humidity);
+void init_i2c();
+
+#endif
+
