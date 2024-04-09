869f171c992b09130df198ab8efaca0dc6262a83
KTurton <159364354+KTurton@users.noreply.github.com>
2024-04-08 21:45:55 -0600

    Update and rename internal_temp_humidity_h.h to internal_temp_humidity.h
    Cleanout

---
+++ internal_temp_humidity.h
@@ -0,0 +1,9 @@
+#ifndef INTERNAL_TEMP_HUMIDITY_H
+#define INTERNAL_TEMP_HUMIDITY_H
+#include <stdio.h>
+
+
+void internal_temp_humidity(float *internal_temperature, float *internal_humidity);
+void init_i2c();
+
+#endif
