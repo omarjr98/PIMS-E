6b102e69772e3099064dcead8ed853c7ce3e6d21
KTurton <159364354+KTurton@users.noreply.github.com>
2024-04-13 23:55:25 -0600

    Update SENSOR_REPORT.h
    Added GPIO, Door Sensor, and corrected Data Report Format

--- SENSOR_REPORT.h
+++ SENSOR_REPORT.h
@@ -1,17 +1,32 @@
+#ifndef SENSOR_REPORT_H
+#define SENSOR_REPORT_H
+#include "internal_temp_humidity.h"
+#include "external_temp_humidity.h"
+#include "accelerometer.h"
+#include "calibration.h"
+#include "wind_sensor.h"
+#include "time.h"
 
 //=================== Sensor Report =========================//
 
 typedef struct SensorReport{
 
     char dateTime[20];
-    float xOffset, yOffset, zOffset;
-    float xAccl, yAccl, zAccl;
-    float external_temperature, external_humidity;
-    float internal_temperature, internal_humidity;
+    float xOffset;
+	float yOffset;
+	float zOffset;
+    float xAccl;
+	float yAccl;
+	float zAccl;
+    float external_temperature;
+    float external_humidity;
+    float internal_temperature;
+	float internal_humidity;
     float wind_speed_mph;
-    // float ultrasound;
+  //float ultrasound;
 
 } SensorReport;
 
 #define SENSOR_REPORT_SIZE (unsigned char)sizeof(SensorReport)
 
+#endif
