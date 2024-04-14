fbc479e19656a9282673d75ac0f9e56cd2003fab
KTurton <159364354+KTurton@users.noreply.github.com>
2024-04-13 23:53:27 -0600

    Update PIMS_C_SHELL_COMMANDS.h
    Added GPIO, Door Sensor, and corrected Data Report Format

--- PIMS_C_SHELL_COMMANDS.h
+++ PIMS_C_SHELL_COMMANDS.h
@@ -1,3 +1,10 @@
+//=================== PIMS_E Report Transfer Commands ================//
 
 #define SINGLE_REPORT_TRANSFER 0xFA
 #define DATA_RECEIVED 0x0E
+
+//================== Maintenance Dump Commands =======================//
+
+#define SD_MAINTENANCE_DUMP 0x0D
+#define READY_SD_DUMP 0x0C
+#define MAINTENANCE_DUMP_COMPLETE 0x0F
