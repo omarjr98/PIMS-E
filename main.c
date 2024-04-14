9388242e5aeaa84d22875178cfd64709f2e2f44f
KTurton <159364354+KTurton@users.noreply.github.com>
2024-04-13 23:52:49 -0600

    Update main.c
    Added GPIO, Door Sensor, and corrected Data Report Format

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "accelerometer.h"
 #include "calibration.h"
 #include "wind_sensor.h"
+#include "door_sensor.h"
 #include "time.h"
 #include <stdio.h>
 #include <unistd.h>
@@ -17,15 +18,14 @@
 
 
 #define FILENAME "/mnt/sdcard/testing.txt"
-#define DATETIME_LENGTH 20
 
 int main() {
 
    int file, rtc_fd;
-    struct rtc_time rtc_tm;
-   char dateTime[DATETIME_LENGTH];
+   struct rtc_time rtc_tm;
+   char dateTime[20];
 
-  //ccelerometer calibration
+  //accelerometer calibration
     initialize_accelerometer(&file);
 
     float xOffset, yOffset, zOffset;
@@ -40,6 +40,8 @@ int main() {
 
 
     while (1) {
+      //Read door status
+    	door_sensor_main();
 
        //Open RTC
         rtc_fd = open(RTC_DEVICE, O_RDONLY);
@@ -58,8 +60,8 @@ int main() {
      close(rtc_fd);
 
         // Get current date and time
-      snprintf(dateTime, DATETIME_LENGTH, "%04d-%02d-%02d %02d:%02d:%02d",
-               rtc_tm.tm_year + 1900, rtc_tm.tm_mon + 1, rtc_tm.tm_mday,
+      snprintf(dateTime, 20, "%02d/%02d/%04d %02d:%02d:%02d",
+    		  	 rtc_tm.tm_mon + 1, rtc_tm.tm_mday, rtc_tm.tm_year + 1900,
                  rtc_tm.tm_hour, rtc_tm.tm_min, rtc_tm.tm_sec);
 
         // Read internal temperature and humidity
@@ -81,10 +83,10 @@ int main() {
 
         // Print sensor data to terminal
         printf("%s\n", dateTime);
-        printf("Internal Temperature: %.2f C\nInternal Humidity: %.2f%\n", internal_temperature, internal_humidity);
-        printf("External Temperature: %.2f C\nInternal Humidity: %.2f%\n", external_temperature, external_humidity);
-        printf("Acceleration X: %.2f g Y: %.2f g Z: %.2f g\n", xAccl, yAccl, zAccl);
-        printf("Wind Speed: %.2f MPH\n", wind_speed_mph);
+        printf("Internal Temperature: %.2fC\nInternal Humidity: %.2f%\n", internal_temperature, internal_humidity);
+        printf("External Temperature: %.2fC\nInternal Humidity: %.2f%\n", external_temperature, external_humidity);
+        printf("Acceleration X: %.2fg Y: %.2fg Z: %.2fg\n", xAccl, yAccl, zAccl);
+        printf("Wind Speed: %.2fMPH\n", wind_speed_mph);
 
 
         // Write sensor data to memory bank
@@ -94,23 +96,39 @@ int main() {
             return 1;
         }
 
-        //fprintf(outputFile, " %s", dateTime);
-        fprintf(outputFile, " %.2fC %.2f%%", internal_temperature, internal_humidity);
-        fprintf(outputFile, " %.2fC %.2f%%", external_temperature, external_humidity);
+        fprintf(outputFile, " %s UTC", dateTime);
+        fprintf(outputFile, " T_i:%.2fC H_i:%.2f%%", internal_temperature, internal_humidity);
+        fprintf(outputFile, " T_e:%.2fC H_e:%.2f%%", external_temperature, external_humidity);
+        fprintf(outputFile, " X_o:%.2fg Y_o:%.2fg Z_o:%.2fg", xOffset, yOffset,zOffset);
         fprintf(outputFile, " X:%.2fg Y:%.2fg Z:%.2fg", xAccl, yAccl, zAccl);
-        fprintf(outputFile, " %.2fMPH\n", wind_speed_mph);
+        fprintf(outputFile, " W:%.2fMPH\n", wind_speed_mph);
         fclose(outputFile);
 
+
+
         //PIMS-C UART
-        SensorReport ttyS1 = {dateTime[20],  xOffset, yOffset, zOffset, xAccl, yAccl, zAccl,external_temperature, external_humidity,internal_temperature, internal_humidity,wind_speed_mph};
-      //SensorReport ttyS1 = { xOffset, yOffset, zOffset, xAccl, yAccl, zAccl,external_temperature, external_humidity,internal_temperature, internal_humidity,wind_speed_mph};
+        SensorReport ttyS1 = {0};
+        		strcpy(ttyS1.dateTime, dateTime);
+        		ttyS1.xOffset = xOffset;
+        		ttyS1.yOffset = yOffset;
+        		ttyS1.zOffset = zOffset;
+        		ttyS1.xAccl = xAccl;
+        		ttyS1.yAccl = yAccl;
+        		ttyS1.zAccl = zAccl;
+        		ttyS1.external_temperature = external_temperature;
+        		ttyS1.external_humidity = external_humidity;
+        		ttyS1.internal_temperature = internal_temperature;
+        		ttyS1.internal_humidity = internal_humidity;
+        		ttyS1.wind_speed_mph = wind_speed_mph;
+
 
         transmitSensorReport(ttyS1);
+
 
         // Delay for 1 second before next reading
         sleep(1);
 
     }
 
-    return 0;
+    return 1;
 }
