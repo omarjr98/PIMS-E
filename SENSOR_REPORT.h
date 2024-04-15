ca10738de2ba6a4fb69a952d8b6dc2185831758e
KTurton <159364354+KTurton@users.noreply.github.com>
2024-04-14 23:34:01 -0600

    Update SENSOR_REPORT.h
    Adjusted floats for PIMS-C interface.

--- SENSOR_REPORT.h
+++ SENSOR_REPORT.h
@@ -1,30 +1,26 @@
+
 #ifndef SENSOR_REPORT_H
 #define SENSOR_REPORT_H
-#include "internal_temp_humidity.h"
-#include "external_temp_humidity.h"
-#include "accelerometer.h"
-#include "calibration.h"
-#include "wind_sensor.h"
-#include "time.h"
 
 //=================== Sensor Report =========================//
 
 typedef struct SensorReport{
-
-    char dateTime[20];
-    float xOffset;
-	float yOffset;
-	float zOffset;
-    float xAccl;
-	float yAccl;
-	float zAccl;
-    float external_temperature;
-    float external_humidity;
-    float internal_temperature;
-	float internal_humidity;
-    float wind_speed_mph;
-  //float ultrasound;
-
+    char dateTime[32];
+    float temperatureExternal;
+    float humidityExternal;
+    float temperatureInternal;
+    float humidityInternal;
+    float accelXMin;
+    float accelXMax;
+    float accelYMin;
+    float accelYMax;
+    float accelZMin;
+    float accelZMax;
+    float accelXOffset;
+    float accelYOffset;
+    float accelZOffset;
+    float windSpeed;
+    float ultrasound;
 } SensorReport;
 
 #define SENSOR_REPORT_SIZE (unsigned char)sizeof(SensorReport)
